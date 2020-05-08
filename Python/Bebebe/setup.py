c76651b7c1c5a579d35331240cce43f258c3919d
ShabanNastya <62402810+ShabanNastya@users.noreply.github.com>
2020-05-08 18:09:24 +0300

    Create setup.py

---
+++ Python/Bebebe/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "Lab-2",
+    version = "0.1",
+    description = "Lab #2, all the tasks",
+    packages = find_packages(),
+    python_requires = ">=3.6"
+)
